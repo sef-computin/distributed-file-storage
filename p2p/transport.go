116693ddebb87a4e518b9cd2f7e3b2b906c24873
Bodgan Yenshin <74347726+sef-computin@users.noreply.github.com>
2024-07-17 12:53:36 +0900

    init

---
+++ p2p/transport.go
@@ -0,0 +1,12 @@
+package p2p
+
+// represents the remote node
+type Peer interface{
+
+}
+
+// anything that handles the communication between the
+// nodes in the network 
+type Transport interface{
+
+}
---
+++ test.go
@@ -0,0 +1,13 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+	fmt.Println("AGAZEGA!")
+}
+
+func Test1(){
+
+}
