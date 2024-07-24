383c2ff6f2fbd26d3a44eb363441fa56fe94f032
sef-computin <74347726+sef-computin@users.noreply.github.com>
2024-07-24 10:16:56 +0300

    Update store.go

--- store.go
+++ store.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
 	"crypto/sha1"
 	"encoding/hex"
 	"io"
@@ -9,65 +11,69 @@ import (
 	"strings"
 )
 
-
 type PathTransformFunc func(string) string
 
-func DefaultPathTransformFunc(key string) string{
-  return key
+func DefaultPathTransformFunc(key string) string {
+	return key
 }
 
-func CASPathTransformFunc(key string) string{
-  hash := sha1.Sum([]byte(key))
-  hashStr := hex.EncodeToString(hash[:])
+func CASPathTransformFunc(key string) string {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
 
-  blocksize := 5
-  sliceLen := len(hashStr) / blocksize
+	blocksize := 5
+	sliceLen := len(hashStr) / blocksize
 
-  paths := make([]string, sliceLen)
+	paths := make([]string, sliceLen)
 
-  for i:=0; i<sliceLen;i++{
-    from, to := i*blocksize, (i*blocksize)+blocksize
-    paths[i] = hashStr[from:to]
-  }
-  return strings.Join(paths, "/")
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blocksize, (i*blocksize)+blocksize
+		paths[i] = hashStr[from:to]
+	}
+	return strings.Join(paths, "/")
 }
 
-type StoreOpts struct{
-  PathTransformFunc PathTransformFunc
+type StoreOpts struct {
+	PathTransformFunc PathTransformFunc
 }
 
-type Store struct{
-  StoreOpts
+type Store struct {
+	StoreOpts
 }
 
-func NewStore(opts StoreOpts) *Store{
-  return &Store{
-    StoreOpts: opts,
-  }
+func NewStore(opts StoreOpts) *Store {
+	return &Store{
+		StoreOpts: opts,
+	}
 }
 
-func (s *Store) writeStream(key string, r io.Reader) error{
-  pathname := s.PathTransformFunc(key)
+func (s *Store) writeStream(key string, r io.Reader) error {
+	pathname := s.PathTransformFunc(key)
 
-  if err := os.MkdirAll(pathname, os.ModePerm); err != nil{
-    return err
-  }
+	if err := os.MkdirAll(pathname, os.ModePerm); err != nil {
+		return err
+	}
 
-  filename := "somefilename"
+	buf := new(bytes.Buffer)
 
-  pathAndFilename := pathname + "/" + filename
+	io.Copy(buf, r)
 
-  f, err := os.Create(pathAndFilename)
-  if err != nil{
-    return err
-  }
+	filenameBytes := md5.Sum(buf.Bytes())
 
-  n, err := io.Copy(f, r)
-  if err != nil{
-    return err
-  }
+	filename := hex.EncodeToString(filenameBytes[:])
+	pathAndFilename := pathname + "/" + filename
 
-  log.Printf("written (%d) bytes to disk: %s", n, pathAndFilename)
+	f, err := os.Create(pathAndFilename)
+	if err != nil {
+		return err
+	}
 
-  return nil
+	n, err := io.Copy(f, buf)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("written (%d) bytes to disk: %s", n, pathAndFilename)
+
+	return nil
 }
